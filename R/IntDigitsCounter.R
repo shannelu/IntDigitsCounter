986429aa4b02d67d9867a1450c3a1260b522cf7d
Shan Lu <lushan0903@gmail.com>
2020-11-18 14:19:14 -0500

    Files Updated

--- R/IntDigitsCounter.R
+++ R/IntDigitsCounter.R
@@ -1,24 +1,18 @@
-DigitCounter_single <- function(data, na.rm = TRUE, verbose = FALSE, ...) {
-  if(na.rm) data <- na.omit(data)
-  if(!is.numeric(data)) {
-    stop('I am so sorry, but this function only works for numeric input!\n',
-         'You have provided an object of class: ', class(data)[1])
-  }
-  # Initialize two variables count and x_tester
-  count <- 0
-  x_tester <- data
-  if(length(x_tester) == 0) {
-    stop('I am so sorry, but this function only works for nonempty input!\n')
-  }
-  if(abs(x_tester) < 1) {
-    if(verbose) cat("Input numberical value", data, "is less than 1\n")
-    return(count) # abs() applied here to consider negative numerical values
-  }
-  else count <- count + 1
-  while (log10(abs(x_tester)) >= 1) {
-    x_tester <- x_tester/10
-    if(verbose) cat("x divided by 10, Times:", count,"and x becomes:", x_tester, "\n")
-    count <- count + 1
-  }
-  return(count)
+#' Count Numbers of Individual Integer Digits in a Vector
+#'
+#' @param x A numeric vector.
+#' @param na.rm A logical input indicating whether missing values should be removed before the computation proceeds.
+#' @param verbose A logical input indicating whether keeps the user up-to-date with messages that print to screen as the function is running.
+#' @param ... Other arguments passed on to methods.
+#'
+#' @return
+#' A vector containing the numbers of integer digits of each number in input.
+#' @export
+#'
+#' @examples
+#' DigitCounter_single(43458.99, verbose=TRUE)
+#' DigitCounter(c(0.123, 1, -245, 4308.99), na.rm = FALSE)
+DigitCounter <- function(x, na.rm = TRUE, verbose = FALSE, ...) {
+  if(na.rm) x <- na.omit(x)  # remove missing value if na.rm is TRUE
+  map_dbl(x, ~ DigitCounter_single(.x, na.rm = na.rm, verbose = verbose))
 }
